7edb97cf5ba92c6f66badd97e8698f6027124f1e
perseidamusic <perseida.musica@gmail.com>
2026-05-20 13:18:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function PerseidaEPK() {
       >
         {/*<div className="absolute inset-0 bg-black/70 backdrop-blur-sm" />*/}
 
-         <div className="relative z-50 max-w-4xl px-6 animate-fadeIn -translate-y-40">
+         <div className="relative z-50 max-w-4xl px-6 animate-fadeIn -translate-y-30">
                  
           <h1 className="text-6xl md:text-8xl font-bold tracking-[0.4em] mb-6 text-slate-100">
             PERSEIDA
